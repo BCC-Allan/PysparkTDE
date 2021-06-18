697eb09edbd48c336c01a56c82b55ba583b8a62e
allanbraun <allanvobraun@gmail.com>
2021-06-18 12:57:20 -0300

    ex5 arrumado

--- src/ex_5.py
+++ src/ex_5.py
@@ -8,20 +8,27 @@ from src.util.helpers import load_rdd, Indexes, save_rdd_to_file
 rdd = load_rdd()
 
 
-def separete_per_key(row):
+def mount_tuple(row: str):
     values = row.split(";")
     year = values[Indexes.YEAR.value]
     cat = values[Indexes.CATEGORY.value]
-    flow = values[Indexes.FLOW.value]
     unit_type = values[Indexes.QUANTITY_NAME.value]
     price = values[Indexes.PRICE.value]
-    key = f"{year}|{flow}|{cat}|{unit_type}"
+    key = f"{year}|{cat}|{unit_type}"
     return key, (int(price), 1)
 
 
-rdd_filtered = rdd.filter(lambda l: l.split(';')[Indexes.COUNTRY.value] == 'Brazil')
-mapped = rdd_filtered.map(separete_per_key)
-rdd_result = mapped.map(lambda t: (t[0], t[1][0] / t[1][1]))
+def filter_brazil_export(row: str):
+    values = row.split(";")
+    flow = values[Indexes.FLOW.value].lower()
+    country = values[Indexes.COUNTRY.value].lower()
+    return flow == 'export' and country == 'brazil'
 
-print(rdd_result.take(5))
-save_rdd_to_file(rdd_qtd_per_flow.coalesce(1), 'ex5')
+
+rdd_filtered = rdd.filter(filter_brazil_export)
+mapped_tuples = rdd_filtered.map(mount_tuple)
+reduced = mapped_tuples.reduceByKey(lambda acc, actual: (acc[0] + actual[0], acc[1] + actual[1]))
+avg_rdd = reduced.map(lambda t: (t[0], t[1][0] / t[1][1]))
+
+print(avg_rdd.take(5))
+save_rdd_to_file(avg_rdd.coalesce(1), 'ex5')
