d320b11301ff613712e54b87fdbaaae82e6142c1
allanbraun <allanvobraun@gmail.com>
2021-06-18 18:24:17 -0300

    ex1 e e 4 arrumados

--- src/ex_4.py
+++ src/ex_4.py
@@ -17,7 +17,6 @@ tuple_rdd = rdd.map(build_value_year_tuple)
 reduced_rdd = tuple_rdd.reduceByKey(lambda acc, actual: (acc[0] + actual[0], acc[1] + actual[1]))
 avg_rdd = reduced_rdd.map(lambda t: (t[0], t[1][0] / t[1][1]))
 
-print(reduced_rdd.take(5))
 print(avg_rdd.take(5))
 
 save_rdd_to_file(avg_rdd.coalesce(1), 'ex4')
