5c8c557755995f1dfb909aa87be4c1e5bc79f547
allanbraun <allanvobraun@gmail.com>
2021-06-18 17:30:12 -0300

    ex6 e 7 arrumados

--- src/ex_6.py
+++ src/ex_6.py
@@ -5,42 +5,33 @@ comoditi com maior preço por / unidade tipo e ano
 
 from src.util.helpers import load_rdd, Indexes, save_rdd_to_file
 
-rdd = load_rdd()
-
 
 def quantity_name_not_empty(row):
     values = row.split(";")
-    qty_name = str(values[Indexes.QUANTITY_NAME.value])
+    qty_name = values[Indexes.QUANTITY_NAME.value]
     return qty_name.lower() != "no quantity"
 
 
-def year_qty_name_per_comm_price_qty(row):
+def build_tuple(row):
     values = row.split(";")
     year = values[Indexes.YEAR.value]
-    commodity = values[Indexes.COMCODE.value]
+    commodity = values[Indexes.COMNAME.value]
     price = values[Indexes.PRICE.value]
     qty_name = values[Indexes.QUANTITY_NAME.value]
-    key = f'{year}_{qty_name}'
-    return key, (int(price), commodity)
+    key = f'{qty_name}_{year}'
+    return key, (commodity, int(price))
 
 
-"""
-The commodity with the highest price per unit type and year;
-comoditi com maior preço por / unidade tipo e ano
-"""
+def reduce_take_max(acc, actual):
+    if acc[1] >= actual[1]:
+        return acc
+    return actual
 
 
-def reduce_take_max(a, b):
-    if a[0] >= b[0]:
-        return a
-    return b
-
-
+rdd = load_rdd()
 first_filter = rdd.filter(quantity_name_not_empty)
-rdd_filtered_2 = first_filter.map(year_qty_name_per_comm_price_qty)
-rdd_qtd_per_flow = rdd_filtered_2.reduceByKey(reduce_take_max)
-print(rdd_qtd_per_flow.take(5))
-#
-# print(rdd_filtered_2.take(5))
-# 2012-'number of items'	commodity:620459-price:9649git
-# save_rdd_to_file(rdd_qtd_per_flow.coalesce(1), 'ex6')
+tuple_rdd = first_filter.map(build_tuple)
+max_rdd = tuple_rdd.reduceByKey(reduce_take_max)
+print(max_rdd.take(5))
+
+save_rdd_to_file(max_rdd.coalesce(1), 'ex6')
--- src/ex_7.py
+++ src/ex_7.py
@@ -3,18 +3,17 @@ The number of transactions per flow type and year.
 """
 from src.util.helpers import load_rdd, Indexes, save_rdd_to_file
 
-rdd = load_rdd()
 
-
-def transaction_flow_type_year(row):
+def tuple_flow_type_year(row):
     values = row.split(";")
     year = values[Indexes.YEAR.value]
     flow_type = values[Indexes.FLOW.value]
-    key = f'{year}_{flow_type}'
+    key = f'{flow_type}_{year}'
     return key, 1
 
 
-rdd_filtered = rdd.map(transaction_flow_type_year)
+rdd = load_rdd()
+rdd_filtered = rdd.map(tuple_flow_type_year)
 trans_flow_type_year = rdd_filtered.reduceByKey(lambda a, b: a + b)
 
 print(trans_flow_type_year.take(5))
